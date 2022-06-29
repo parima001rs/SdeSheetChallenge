670fb2e324a398811861801eddf714655d6b7c9e
Parima Kashyap <parima001rs@gmail.com>
2022-06-29 20:08:27 +0530

    day15

--- KthPermutationSequence.cpp
+++ KthPermutationSequence.cpp
@@ -1,13 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void helperBrute(vector<int>&v, int ind, int k, set<vector<int>>&s){
-
+void helperBrute(string& s, int ind, vector<string>&res){
+    if(ind == s.size()){
+        res.push_back(s);
+        return;
+    }
+    for(int i=index; i<s.size(); i++){
+        swap(s[i], s[ind]);
+        helperBrute(s, ind+1, res);
+        swap(s[i], s[ind]);
+    }
 }
 
-//BruteForce using recursion TC:O(n!)
+//BruteForce using recursion TC:O(N! * N) +O(N! Log N!) & SC: O(N)
+//The recursion takes O(N!)  time because we generate every possible permutation and another O(N)  time is required to make a deep copy and store every sequence in the data structure. Also, O(N! Log N!)  time required to sort the data structure
 string solveBrute(int n, int k){
-   
+   string s;
+   vector<string>res;
+   for(int i=1; i<=n; i++){
+    s.push_back(i+'0');
+   }
+   helperBrute(s, 0, res);
+   sort(res.begin(), res.end());
+   auto it = res.begin() + (k-1);
+   return *it;
 }
 
 //Optimal : must remember intuition
